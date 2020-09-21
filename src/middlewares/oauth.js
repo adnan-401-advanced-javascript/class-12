6135e02543aff9cbec434af333f9842e3ba4d944
amuammer <amuammer@students.iugaza.edu.ps>
2020-09-21 16:44:12 +0300

    fix oath google bad request

--- src/middlewares/oauth.js
+++ src/middlewares/oauth.js
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 /* eslint-disable no-use-before-define */
 const superagent = require("superagent");
 const users = require("../users.js");
@@ -10,64 +11,59 @@ module.exports = async function (req, res, next) {
     const { code } = req.query;
     console.log("__CODE__:", code);
 
-    let remoteToken = await exchangeCodeForToken(code);
-    console.log('__GOOGLE TOKEN__:', remoteToken);
+    const remoteToken = await exchangeCodeForToken(code);
+    console.log("__GOOGLE TOKEN__:", remoteToken);
 
-    let remoteUser = await getRemoteUserInfo(remoteToken);
-    console.log('__GOOGLE USER__:', remoteUser);
+    const remoteUser = await getRemoteUserInfo(remoteToken);
+    console.log("__GOOGLE USER__:", remoteUser);
 
     const [user, token] = await getUser(remoteUser);
     req.token = token;
     req.user = user;
 
-    console.log('__LOCAL USER__:', user);
+    console.log("__LOCAL USER__:", user);
     next();
-  } catch(err) {
+  } catch (err) {
     next(`Error: ${err}`);
   }
-}
+};
 
 // this will use the access_token github api endpoint
 async function exchangeCodeForToken(code) {
-  let tokenResponse = await superagent.post('https://www.googleapis.com/oauth2/v4/token')
+  const tokenResponse = await superagent.post("https://www.googleapis.com/oauth2/v4/token")
     .send({
       code,
       client_id: CLIENT_ID,
       client_secret: CLIENT_SECRET,
       redirect_uri: API_SERVER,
-      grant_type: 'authorization_code'
-    })
+      grant_type: "authorization_code",
+    });
 
-    console.log("exchangeCodeForToken Out");
+  console.log("exchangeCodeForToken Out");
 
-  let access_token = tokenResponse.body.access_token;
+  const { access_token } = tokenResponse.body;
   return access_token;
 }
 
 // this will use the user api endpoint to get user info/repo info
 async function getRemoteUserInfo(token) {
   // this will use the access token to get user details
-  console.log("before");
-  let userResponse = await superagent.get(`https://oauth2.googleapis.com/tokeninfo?id_token=${token}`)
-    .set('user-agent', 'express-app')
-    .set('Authorization', `token ${token}`);
-    console.log("after");
+  const userResponse = await superagent.get(`https://oauth2.googleapis.com/tokeninfo?access_token=${token}`)
+    .set("user-agent", "express-app")
+    .set("Authorization", `token ${token}`);
 
-  let user = userResponse.body;
+  const user = userResponse.body;
   return user;
 }
 
 async function getUser(remoteUser) {
-  // this will actually save the user to the db and return user details from the db
-  let userRecord = {
-    username: remoteUser.login,
-    password: 'canbeanything'
-  }
+  const userRecord = {
+    username: remoteUser.email,
+    password: "canbeanything",
+  };
 
-  let user = await users.save(userRecord);
-  // this is meant for us to generate a final user token to access routes in our app
-  // tomorrow: this is will be used in the format of a Bearer Authentication Token
-  let token = users.generateToken(user);
+  const user = await users.save(userRecord);
+  const token = users.generateToken(user);
 
   return [user, token];
 }
